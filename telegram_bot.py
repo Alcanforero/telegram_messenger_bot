88442e0b3aabb25c1230b255ad3a16736cc733d0
Benjamín Medina Cabrera <alcanforero.bmc@gmail.com>
2021-02-11 17:36:58 +0000

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,21 +1,30 @@
 import requests
+
 from config import TELEGRAM_SEND_MESSAGE_URL
 
 class TelegramBot:
+
     def __init__(self):
         self.chat_id = None
         self.text = None
 
     def parse_webhook_data(self, data):
         message = data['message']
+
+        f = open("file.txt", "a")
+        f.write(message)
+        f.close()
+
         self.chat_id = message['chat']['id']
         self.incoming_message_text = message['text'].lower()
 
     def action(self):
         success = None
+
         msg = self.incoming_message_text
         self.outgoing_message_text = msg
         success = self.send_message()
+
         return success
 
     def send_message(self):
